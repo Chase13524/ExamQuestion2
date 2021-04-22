9e9bbe5e81a81423bc7f57a6ee5334ae753a7428
Chase <chasesbrown24@gmail.com>
2021-04-22 19:49:57 -0400

    finish

--- program.cpp
+++ program.cpp
@@ -1,12 +1,27 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+using std::ifstream;
 using std::cout;
 using std::string;
 int countLine(string pName);
 int countChar(string pName);
 int main(int argc, char *argv[]){
-	string a = "absdbasbdabsdasdybaybsdybasd";
-	cout << countChar(a);
+	if (argc == 1){
+		countLine("Ohio University");
+		countChar("Athens");
+	} else if (argc == 2){
+		ifstream in_file;
+		in_file.open(argv[1], std::ios_base::in);
+		if (!in_file.is_open()){
+			cout << "Not a valid file.\n";
+			return;
+		}
+		string words_in_file = string((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
+		countLine(words_in_file);
+		countChar(words_in_file);
+		in_file.close();
+	}
 }
 
 int countLine(string pName){
@@ -16,7 +31,7 @@ int countLine(string pName){
 			sum++;
 		}
 	}
-	cout << sum << "Lines\n";
+	cout << sum << " Lines\n";
 	return sum;
 }
 int countChar(string pName){
@@ -26,6 +41,6 @@ int countChar(string pName){
 			sum++;
 		}
 	}
-	cout << sum << "Characters\n";
+	cout << sum << " Characters\n";
 	return sum;
 }
