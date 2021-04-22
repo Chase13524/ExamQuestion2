13dfff7da1da5c8338ab3866807c1fb77f16114e
Chase <chasesbrown24@gmail.com>
2021-04-22 19:32:18 -0400

    countChar

--- program.cpp
+++ program.cpp
@@ -3,9 +3,10 @@
 using std::cout;
 using std::string;
 int countLine(string pName);
+int countChar(string pName);
 int main(int argc, char *argv[]){
-	string a = "a\na\na\na\nr";
-	cout << countLine(a);
+	string a = "absdbasbdabsdasdybaybsdybasd";
+	cout << countChar(a);
 }
 
 int countLine(string pName){
@@ -15,8 +16,16 @@ int countLine(string pName){
 			sum++;
 		}
 	}
+	cout << sum << "Lines\n";
 	return sum;
 }
 int countChar(string pName){
-
+	int sum = 0;
+	for (auto i = pName.begin(); i < pName.end(); i++){
+		if ((int)*i > 32 && (int)*i < 127){
+			sum++;
+		}
+	}
+	cout << sum << "Characters\n";
+	return sum;
 }
